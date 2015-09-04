8bc113fe0104da8c04744c64396df1eae9b173b7
ThomasLange <thomashlange@gmail.com>
2015-09-04 15:47:46 +0200

    Fixed Affine

--- src/Runner.java
+++ src/Runner.java
@@ -1,35 +1,29 @@
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 /**
  * Created by Thomas on 27-08-2015.
  */
 public class Runner {
     public static void main(String[] args) throws Exception{Map<CharPair, Integer> seqMatrix = new HashMap<CharPair, Integer>();
-
-
-        MatrixParser mp = new MatrixParser();
-        List<Integer> li = mp.getCostMatrix("costMatrix.txt");
-
-        seqMatrix.put(new CharPair('A', 'A'), -10);
-        seqMatrix.put(new CharPair('A', 'C'), -2);
-        seqMatrix.put(new CharPair('A', 'G'), -5);
-        seqMatrix.put(new CharPair('A', 'T'), -2);
-        seqMatrix.put(new CharPair('C', 'A'), -2);
-        seqMatrix.put(new CharPair('C', 'C'), -10);
-        seqMatrix.put(new CharPair('C', 'G'), -2);
-        seqMatrix.put(new CharPair('C', 'T'), -5);
-        seqMatrix.put(new CharPair('G', 'A'), -5);
-        seqMatrix.put(new CharPair('G', 'C'), -2);
-        seqMatrix.put(new CharPair('G', 'G'), -10);
-        seqMatrix.put(new CharPair('G', 'T'), -2);
-        seqMatrix.put(new CharPair('T', 'A'), -2);
-        seqMatrix.put(new CharPair('T', 'C'), -5);
-        seqMatrix.put(new CharPair('T', 'G'), -2);
-        seqMatrix.put(new CharPair('T', 'T'), -10);
+        seqMatrix.put(new CharPair('A', 'A'), 0);
+        seqMatrix.put(new CharPair('A', 'C'), 5);
+        seqMatrix.put(new CharPair('A', 'G'), 2);
+        seqMatrix.put(new CharPair('A', 'T'), 5);
+        seqMatrix.put(new CharPair('C', 'A'), 5);
+        seqMatrix.put(new CharPair('C', 'C'), 0);
+        seqMatrix.put(new CharPair('C', 'G'), 5);
+        seqMatrix.put(new CharPair('C', 'T'), 2);
+        seqMatrix.put(new CharPair('G', 'A'), 2);
+        seqMatrix.put(new CharPair('G', 'C'), 5);
+        seqMatrix.put(new CharPair('G', 'G'), 0);
+        seqMatrix.put(new CharPair('G', 'T'), 5);
+        seqMatrix.put(new CharPair('T', 'A'), 5);
+        seqMatrix.put(new CharPair('T', 'C'), 2);
+        seqMatrix.put(new CharPair('T', 'G'), 5);
+        seqMatrix.put(new CharPair('T', 'T'), 0);
         int gapcost = 5;
 
-        AffineSequenceAligner seqAligner = new AffineSequenceAligner(seqMatrix, gapcost, 0);
+        AffineSequenceAligner seqAligner = new AffineSequenceAligner(seqMatrix, 5, 5);
         FastaParser fastaParser = new FastaParser("seq1.fasta");
         char[] seq1 = fastaParser.parse("Seq1").toCharArray();
         fastaParser = new FastaParser("seq2.fasta");
